b7d173a941e2ae1970b8e3b4bc66ef79d61b2dd8
Vishal N <itvishal.n@muthootgroup.com>
2026-06-03 14:59:49 +0530

    Latest

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using mhil_net.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace mhil_net.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
